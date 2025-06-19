5129430462b5311998242e17d14a8257c96dfd08
Phúc Hậu <haunp230801@gmail.com>
2025-06-19 10:42:56 +0700

    hiển thị

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>Danh sách passport chờ xác nhận</h2>
+    <div v-for="(p, idx) in passports" :key="idx" class="passport-card">
+      <img
+        :src="backendUrl + '/api/passport-img/' + p.image_photo"
+        width="120"
+        alt="Photo"
+      />
+      <img
+        :src="backendUrl + '/api/passport-img/' + p.image_vis"
+        width="120"
+        alt="Passport"
+      />
+      <div>
+        <div>Mã ICAO/MRZ:</div>
+        <pre style="font-family:monospace; background:#eee; padding:10px">{{ p.icao_mrz }}</pre>
+      </div>
+      <button @click="confirm(p)">Xác nhận (Confirm)</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      passports: [],
+      backendUrl: "http://localhost:4000",
+    };
+  },
+  methods: {
+    async fetchPassports() {
+      const res = await axios.get(this.backendUrl + "/api/pending-passports");
+      this.passports = res.data;
+    },
+    async confirm(p) {
+      const res = await axios.post(this.backendUrl + "/check-passport", p);
+      alert("Kết quả: " + JSON.stringify(res.data));
+    },
+  },
+  mounted() {
+    this.fetchPassports();
+    setInterval(() => {
+      this.fetchPassports();
+    }, 10000);
+  },
+};
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.passport-card {
+  border: 1px solid #eee;
+  padding: 12px;
+  margin: 12px 0;
+  display: flex;
+  gap: 16px;
+  align-items: center;
 }
 </style>
