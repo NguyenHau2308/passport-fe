22b5131270c7a38a7e9f1f9ff362174884694848
Phúc Hậu <haunp230801@gmail.com>
2025-07-01 14:13:48 +0700

    refactor code

--- src/utils/keycloak.js
+++ src/utils/keycloak.js
@@ -18,9 +18,6 @@ export function getUserRoles() {
     }
 }
 export function logoutKeycloak() {
-    // console.log('Logging out Keycloak...');
-    // console.log('kc_token:', localStorage.getItem('kc_token'));
-    // console.log('kc_refresh_token:', localStorage.getItem('kc_refresh_token'));
     fetch(`${process.env.VUE_APP_BE_URL}/auth/logout`, {
         method: 'POST',
         credentials: 'include',
@@ -32,20 +29,14 @@ export function logoutKeycloak() {
             refresh_token: localStorage.getItem('kc_refresh_token')
         })
     })
-        .then(res => {
-            console.log('BE logout status:', res.status);
+        .then(() => {
             localStorage.removeItem('kc_token');
             localStorage.removeItem('kc_refresh_token');
-            console.log('Token đã remove:',
-                'kc_token:', localStorage.getItem('kc_token'),
-                'kc_refresh_token:', localStorage.getItem('kc_refresh_token')
-            );
-            // setTimeout(() => {
-            //     window.location = '/';
-            // }, 10000);
+            window.location = '/';
         })
-        .catch(e => {
-            console.error('Logout error:', e);
+        .catch(() => {
+            window.location = '/';
         });
 }
+
 
